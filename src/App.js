deaea6332bf6b852e9b553668477553fa040691c
Cody <cemt5462@gmail.com>
2023-01-26 17:15:46 -0600

    Started Styling but need to resolve issue to reflect styling to app.js.

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import DisplayPosts from './Components/DisplayPosts/DisplayPosts';
 import CreatePostForm from './Components/CreatePostForm/CreatePostForm';
 import NavBar from './Components/NavBar/NavBar';
-
+import './App.css';
 
 
 /* Created App function to show a table in the browser with hard coded instanceof. 
@@ -20,11 +20,26 @@ function App() {
   }
 
   return (
-    <div>
-      <NavBar/>
-      <CreatePostForm addNewPostProp={addNewPost}/>
-      <DisplayPosts parentPosts={posts} />
-
+    <div className='container-fluid'>
+      <div className='row'>
+        <div className='-md-6 mx-auto'>
+          <div className='nav-border'>
+            <NavBar/>
+          </div>
+        </div>
+      </div>
+      <body>
+        <div className='row'>
+          <div className='md-6 mx-auto'>
+            <div className='border-box'>
+              <CreatePostForm addNewPostProp={addNewPost}/>
+            </div>
+            <div className='border-box'>
+              <DisplayPosts parentPosts={posts} />
+            </div>
+          </div>
+        </div>
+      </body>
     </div>
   );
 }
